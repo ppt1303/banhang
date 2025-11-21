804c62bbc1ea706e5f94dec2f237646b6513c294
Phạm Pu Tinl <phamputinl2005@gmail.com>
2025-11-21 10:03:50 +0700

    login

--- backend/app/api.py
+++ backend/app/api.py
@@ -1,8 +1,21 @@
-from fastapi import FastAPI
-from fastapi.responses import FileResponse,Jinja2Templates
+from urllib import request
+from fastapi import FastAPI, Request, Form
+from fastapi.templating import Jinja2Templates
+from pathlib import Path
+from databases.database import login_col
 
 
 app = FastAPI()
+cur_path=Path(__file__).resolve().parent.parent.parent
+templates=Jinja2Templates(directory=cur_path/"frontend/templates")
+
 @app.get("/")
-async def login():
-    return FileResponse("login.html")
+async def load(request: Request):
+    return templates.TemplateResponse(request= request, name="login.html")
+
+@app.post("/submit")
+async def login(request: Request, nm: str = Form(...), pwd: str = Form(...)):
+    if login_col.find_one({"name":nm, "password": pwd}):
+        return templates.TemplateResponse(request=request,name="submit.html")
+    else:
+        return {"status":"Login Failed"}
